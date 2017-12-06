ab4b6157baf4766c18369cd4b8aba134c8594963
Meiliang Dong <dongmeilianghy@sina.com>
2017-12-06 16:11:16 +0800

    Added models to Abstract Factory playground.

---
+++ Creational/Abstract Factory.playground/Sources/Door.swift
@@ -0,0 +1,9 @@
+public class Door {
+    var roomFrom: Room
+    var roomTo : Room
+    
+    public init(roomFrom: Room, roomTo: Room) {
+        self.roomFrom = roomFrom
+        self.roomTo = roomTo
+    }
+}
